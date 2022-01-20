87a6666aeb5d52430d6a8c275551f074265ed886
spiessenst <spiessenst@gmail.com>
2022-01-20 13:51:44 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once "lib/autoload.php";
+
+PrintHead();
+?>
+
+      <main>
+
+   <?php  PrintTop() ; ?>
+
+    <section class="middle">
+
+    </section>
+
+    </main>
+    <?php  PrintFooter() ;
