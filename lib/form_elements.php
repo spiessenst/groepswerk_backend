1eb7f3c28b6087fcb5c4a7a4a86141efe845487d
spiessenst <spiessenst@gmail.com>
2022-01-25 20:15:03 +0100

    add top

--- lib/form_elements.php
+++ lib/form_elements.php
@@ -1,30 +1,8 @@
 <?php
 require_once "autoload.php";
 
-function MakeSelect( $fkey, $value, $sql )
-{
-    $select = "<select id=$fkey name=$fkey value=$value>";
-    $select .= "<option value='0'></option>";
 
-    $data = GetData($sql);
-
-    foreach ( $data as $row )
-    {
-        if ( $row[0] == $value ) $selected = " selected ";
-        else $selected = "";
-
-        $select .= "<option $selected value=" . $row[0] . ">" . $row[1] . "</option>";
-    }
-
-    $select .= "</select>";
-
-    return $select;
-}
-
-function GenreList($sql){
-
-    $data = GetData($sql);
-
+function GenreList($data){
 
     $list  = '<section class="genre_links"> <ul>';
 
@@ -40,9 +18,3 @@ function GenreList($sql){
 
 
 }
-
-function MakeCheckbox( )
-{
-
-}
-
