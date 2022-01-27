d896e1f335db2721841afaebc25ee48225fb770c
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-01-27 10:52:38 +0100

    album data is taken from the database

--- index.php
+++ index.php
@@ -6,20 +6,32 @@ PrintHead();
 
     <main>
 
-   <?php  PrintTop() ; ?>
+        <?php PrintTop(); ?>
 
-    <section class="middle">
-    <?php PrintMiddle() ; ?>
-    </section>
+        <section class="middle">
+            <div class="albumgrid">
+                <?php
+                //get data
+                $data = getData("select * from album inner join artist a on album.alb_art_id = a.art_id");
+                //get template
+                $template = file_get_contents("templates/albumgrid.html");
+
+                //merge
+                $output = MergeViewWithData($template, $data);
+                print $output;
+
+                ?>
+                <!----- <div class="pagenumbers"><p>1 2 3 ></p></div></div> --->
+        </section>
 
     </main>
 
-    <?php
+<?php
 
-    $data = getData("select * from genre" );
-    print GenreList($data) ?>
+$data = getData("select * from genre");
+print GenreList($data) ?>
 
-    <?php  PrintFooter() ;
+<?php PrintFooter();
 
 
 
