0ac56b2dd9e1b34604167d7d2202b303baca757a
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-03 17:23:31 +0100

    added intro page

---
+++ intro.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors', 1);
+require_once "lib/autoload.php";
+
+PrintHead();
+?>
+
+    <main>
+
+        <?php PrintTop(); ?>
+
+        <section class="middle">
+
+        </section>
+
+    </main>
+
+<?php
+//$data = getData("select * from genre");
+//print GenreList($data)  //print the genres?>
+
+<?php //PrintFooter();
+
+
+
+
