1b7b3a0ea69a7710d36f3030ceaf5d5273092584
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-09 12:33:52 +0100

    trying to make the album information appear in the corresponding input fields - is not ready yet!!!

--- update.php
+++ update.php
@@ -12,20 +12,27 @@ PrintHead();
 
         <section class="middle">
             <?php
-            $data = getData("select * from genre");
+            $sql = "select * from album 
+                    inner join track t on album.alb_id = t.tr_alb_id
+                    inner join artist a on album.alb_art_id = a.art_id
+                    inner join album_genre ag on album.alb_id = ag.alb_id
+                    inner join genre g on ag.gr_id = g.gr_id
+                    where album.alb_id = " . $_GET['alb_id'];
 
+            $data = getData($sql);
 
-            $extra_elements['select_genre'] = GenreSelect($data);
-            $extra_elements['csrf_token'] = GenerateCSRF();
-
-
-            $template =  file_get_contents("templates/update.html");
-
-            $output =  MergeViewWithExtraElements( $template , $extra_elements);
+            $template = file_get_contents("templates/update.html");
+            $output = MergeViewWithData($template, $data);
             print $output;
+
+//            $extra_elements['select_genre'] = GenreSelect($data);
+//            $extra_elements['csrf_token'] = GenerateCSRF();
+//
+//            $output_extraelements = MergeViewWithExtraElements($template, $extra_elements);
+//            print $output_extraelements;
             ?>
 
-        
+
         </section>
 
     </main>
